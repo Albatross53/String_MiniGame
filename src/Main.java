e3f58cb737d9efee5eb8bca03871797b446fe013
alexje53 <hyebin199953@gmail.com>
2023-02-21 15:41:27 +0900

    git Main.java change

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+	String name = "hello";
         String input_1 = "";
         String input_2 = "";
 
